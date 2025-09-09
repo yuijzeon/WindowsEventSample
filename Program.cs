1f21fbede1b801b717e694ccc84ac61c91b46bcd
tintin <tintin@babyduck.tw>
2025-09-09 22:14:54 +0800

    Add Windows event handling functionality and helper methods

--- Program.cs
+++ Program.cs
@@ -1,9 +1,65 @@
-﻿namespace WindowsEventSample;
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Accessibility;
+
+namespace WindowsEventSample;
 
 public static class Program
 {
     public static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        var onWinEvent = new WINEVENTPROC(
+            (_, eventId, hwnd, _, _, idEventThread, eventTime) =>
+            {
+                var eventName = WindowHelper.GetEventName(eventId);
+                var windowTitle = WindowHelper.GetTitleText(hwnd);
+                if (string.IsNullOrEmpty(eventName) || string.IsNullOrEmpty(windowTitle))
+                {
+                    return;
+                }
+
+                var tick = Environment.TickCount & int.MaxValue;
+                var timeText = $"{DateTime.Now.AddMilliseconds(eventTime - tick):s}";
+                Console.WriteLine(
+                    $"{timeText} [{eventName, -27}] hwnd={hwnd, -9:X} tid={idEventThread, -6} {windowTitle}"
+                );
+            }
+        );
+
+        var loopThreadId = 0U;
+        var started = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        var stopped = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        _ = Task.Run(() =>
+        {
+            loopThreadId = PInvoke.GetCurrentThreadId();
+            var hook = PInvoke.SetWinEventHook(
+                PInvoke.EVENT_MIN,
+                PInvoke.EVENT_MAX,
+                HMODULE.Null,
+                onWinEvent,
+                0,
+                0,
+                PInvoke.WINEVENT_OUTOFCONTEXT
+            );
+
+            started.SetResult();
+            while (PInvoke.GetMessage(out var msg, HWND.Null, 0, 0))
+            {
+                PInvoke.DispatchMessage(msg);
+            }
+
+            PInvoke.UnhookWinEvent(hook);
+            stopped.SetResult();
+        });
+
+        await started.Task;
+
+        Console.WriteLine("SetWinEventHook OK, Try Click Other Window...");
+        Console.ReadLine();
+
+        PInvoke.PostThreadMessage(loopThreadId, PInvoke.WM_QUIT, default, default);
+        await stopped.Task;
+
+        Console.WriteLine("Program Exited.");
     }
 }
---
+++ WindowHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace WindowsEventSample;
+
+public static class WindowHelper
+{
+    private static readonly Dictionary<uint, string> WinEventNameMapping = new();
+
+    static WindowHelper()
+    {
+        const BindingFlags flags = BindingFlags.Public | BindingFlags.Static;
+        foreach (var f in typeof(PInvoke).GetFields(flags))
+        {
+            if (!f.Name.StartsWith("EVENT_"))
+            {
+                continue;
+            }
+
+            var val = f.GetRawConstantValue();
+            if (val != null)
+            {
+                WinEventNameMapping[(uint)val] = f.Name;
+            }
+        }
+    }
+
+    public static string? GetEventName(uint id)
+    {
+        return WinEventNameMapping.GetValueOrDefault(id);
+    }
+
+    public static string? GetTitleText(HWND hwnd)
+    {
+        if (hwnd == HWND.Null || !PInvoke.IsWindow(hwnd))
+        {
+            return null;
+        }
+
+        var len = PInvoke.GetWindowTextLength(hwnd);
+        if (len <= 0)
+        {
+            return null;
+        }
+
+        Span<char> buf = stackalloc char[len + 1];
+        var written = PInvoke.GetWindowText(hwnd, buf);
+        return written > 0 ? new string(buf[..written]) : null;
+    }
+}
